7a008ca827d7d535380ecfc149d8d978855a143a
Nicolas <nicolassilverii@gmail.com>
2025-10-25 20:23:04 -0300

    Autenticación login, registro, persistencia & feedback visual

--- biblioteca-inteligente-react/src/components/Modal.jsx
+++ biblioteca-inteligente-react/src/components/Modal.jsx
@@ -10,17 +10,17 @@ const Modal = ({ mensaje, tipo = "info", onClose }) => {
   const iconos = {
     info: "ℹ️",     // Informativo
     error: "❌",    // Error
-    confirm: "✅",  // Confirmación o éxito
+    confirm: "✅",  // Confirmación 
   };
 
   return (
     <div className="popup-overlay">
       <div className={`popup-contenido modal-${tipo}`}>
         <p className="modal-mensaje">
-          {/* Mostramos el ícono correspondiente y el mensaje */}
+          {/* Se muestra el ícono correspondiente y el mensaje */}
           <span className="modal-icono">{iconos[tipo]}</span> {mensaje}
         </p>
-        {/* Botón para cerrar el modal */}
+        {/* botón cerrar modal */}
         <button className="boton boton-primario" onClick={onClose}>
           Cerrar
         </button>
